9d9aef67f0b097b1a24764cfe2d8ce6b5d0f9f84
mThanuj <mullagurithanuj0@gmail.com>
2025-01-18 08:40:51 +0530

    some

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,5 +1,18 @@
 import "dotenv/config";
 
+const requiredEnvVars = [
+  "WAIFU_API_BASE_URL",
+  "WAIFU_API_TOKEN",
+  "DATABASE_URL",
+];
+
+for (const varName of requiredEnvVars) {
+  if (!process.env[varName]) {
+    console.error(`Missing required environment variable: ${varName}`);
+    process.exit(1);
+  }
+}
+
 const config = {
   env: {
     waifuApi: {
